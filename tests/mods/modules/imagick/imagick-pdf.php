3153b329f143f0d0410b70e4866d0685569fdf46
cytopia <cytopia@everythingcli.org>
2020-08-08 11:08:05 +0200

    Added imagick PDF support (via ghostscript)

---
+++ tests/mods/modules/imagick/imagick-pdf.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This page should print 'OK' if everything works,
+ * 'FAIL' or nothing if an error occured.
+ */
+ini_set('display_startup_errors', 1);
+ini_set('display_errors', 1);
+error_reporting(-1);
+
+$PHP_VERSION = str_replace('-dev', '', PHP_VERSION);
+
+// Only available since 5.3.0 (PHP 5.3 and 5.4 segfaults)
+if (version_compare($PHP_VERSION, '5.5.0', '<')) {
+        echo 'SKIP';
+        exit(0);
+}
+// FIXME: Currently not supported on PHP 8
+if (version_compare($PHP_VERSION, '8.0.0', '>=')) {
+        echo 'SKIP';
+        exit(0);
+}
+
+$img = new Imagick();
+
+if ($img->readImage('dummy.pdf') !== True) {
+	echo 'FAIL: newImage()';
+	exit(1);
+}
+echo 'OK';
